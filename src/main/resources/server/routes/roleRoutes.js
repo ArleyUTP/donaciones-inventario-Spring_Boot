8853351f47b4098533f02d4f3ab7ff9ba91050bc
Arley Ticse <u23238849@utp.edu.pe>
2025-07-16 20:06:25 -0500

    Remove server source and dependencies
    Deleted all files related to the server implementation, including controllers, models, configuration, Dockerfile, and all node_modules dependencies. This likely indicates a migration, deprecation, or major refactor of the server-side codebase.

--- src/main/resources/server/routes/roleRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const roleController = require('../controllers/roleController');
-
-router.get('/roles', roleController.getRoles);
-
-module.exports = router;
